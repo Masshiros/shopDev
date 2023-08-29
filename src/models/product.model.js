4edb87d921824b922cceffbe037cf0fb3978d368
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2023-08-29 10:17:57 +0700

    Update Product Model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { Schema, model } = require("mongoose"); // Erase if already required
+const slugify = require("slugify");
 const DOCUMENT_NAME = "Product";
 const COLLECTION_NAME = "Products";
 
@@ -10,6 +11,7 @@ const productSchema = new Schema(
     product_name: { type: String, required: true },
     product_thumb: { type: String, required: true },
     product_description: String,
+    product_slug: String,
     product_price: { type: Number, required: true },
     product_quantity: { type: Number, required: true },
     product_type: {
@@ -19,12 +21,30 @@ const productSchema = new Schema(
     },
     product_shop: { type: Schema.Types.ObjectId, ref: "Shop" },
     product_attributes: { type: Schema.Types.Mixed, required: true },
+    product_ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be under 5.0"],
+      set: (val) => Math.round(val * 10) / 10,
+    },
+    product_variations: {
+      type: Array,
+      default: {},
+    },
+    isDraft: { type: Boolean, default: true, index: true, select: false },
+    isPublished: { type: Boolean, default: true, index: true, select: false },
   },
   {
     collection: COLLECTION_NAME,
     timestamps: true,
   }
 );
+// document middleware: runs before .save() and .create()
+productSchema.pre("save", function (next) {
+  this.product_slug = slugify(this.product_name, { lower: true });
+  next();
+});
 // define the product type= clothing
 const clothingSchema = new Schema(
   {
